83d66e06a459db8aa2f3b70a06c2e6224b32a5b4
ChetanM1555 <126090346+ChetanM1555@users.noreply.github.com>
2023-03-04 21:09:07 +0200

    cleaned up some of the code

--- space_python_game/space_game.py
+++ space_python_game/space_game.py
@@ -8,14 +8,7 @@ pygame.init()
 display_width = 800
 display_height = 600
 
-# gameDisplay = pygame.display.set_mode((display_width, display_height))
-# pygame.display.set_caption('Practice')
-
 window = pygame.display.set_mode((display_width,display_height))
-
-# bg_img = pygame.transform.scale(bg_img,(display_width,display_height))
-
-
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -26,14 +19,18 @@ grey3 = (50,50,50)
 darkgrey = (50,50,50)
 
 clock = pygame.time.Clock()
-closed = False
+
 bg = pygame.image.load('space.gif')
 snoop = pygame.image.load('k.png')
 width = 120
 
 
+#-----------------------------------------------------------
 
 def draw1(x,y, height, width):
+    """
+    draws the obstacles that the user has to dodge
+    """
     pygame.draw.circle(window, grey2, (x+height/2,y+width/2), width/2)
     pygame.draw.circle(window, grey, (x+height/2+10,y+width/2+10), width/2+5)
     pygame.draw.circle(window, grey, (x+height/2+10,y+width/2-7), width/2-3)
@@ -43,138 +40,123 @@ def draw1(x,y, height, width):
     pygame.draw.circle(window, grey3, (x,y+width), width/9)
     pygame.draw.circle(window, grey2, (x+width,y+width +20), width/12)
 
-
-    # pygame.draw.rect(window, green, [x, y, 7, 20])
-
+#-----------------------------------------------------------
 
 def move(x, y):
+    """
+    moves the user
+    """
     window.blit(snoop, (x, y))
 
+#-----------------------------------------------------------
 
+def game_loop():
+    closed = False
+    x = (display_width * 0.42)
+    y = (display_height * 0.8)
+    y_change = 0
+    x_change = 0
+    obst_speed = 5
+    obst_speed2 = -5
 
-x = (display_width * 0.42)
-y = (display_height * 0.8)
-y_change = 0
-x_change = 0
-obst_speed = 5
-obst_speed2 = -5
+    obst_x = random.randrange(0,display_width)
+    obst_y = -600
+    height = 100
+    width1 = 100
+    count = 0
 
-obst_x = random.randrange(0,display_width)
-obst_y = -600
-height = 100
-width1 = 100
-count = 0
-i = 0
+    while closed == False:
+        window.blit(bg, (0,0))
 
-lives = 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                closed = True
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    y_change = -5
+                    x_change = 0
 
-# pygame.draw.rect(gameDisplay, black, (400, 400, x, y))
+                if event.key == pygame.K_DOWN:
+                    y_change = 5
+                    x_change = 0
 
-while closed == False:
-    window.blit(bg, (0,0))
-    # window.fill((0,0,0))
-    # window.blit(bg,(i,0))
-    # window.blit(bg,(width+i,0))
-    # if (i==-width):
-    #     window.blit(bg,(width+i,0))
-    #     i=0
-    # i-=1
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            closed = True
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                y_change = -5
-                x_change = 0
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                    y_change = 0
 
-            if event.key == pygame.K_DOWN:
-                y_change = 5
-                x_change = 0
-
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-                y_change = 0
-
-            if event.key == pygame.K_RIGHT:
-                x_change = 5
-                y_change = 0
-    x += x_change
-    y += y_change
-    if x > display_width - width or x < 0:
-        x_change = 0
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+                    y_change = 0
         x += x_change
         y += y_change
-        move(x, y)
-        pass
-    if y > display_height - width or y < 0:
-        y_change = 0
-        x += x_change
-        y += y_change
-        move(x, y)
-        pass
+        if x > display_width - width or x < 0:
+            x_change = 0
+            x += x_change
+            y += y_change
+            move(x, y)
+            pass
+        if y > display_height - width or y < 0:
+            y_change = 0
+            x += x_change
+            y += y_change
+            move(x, y)
+            pass
 
 
-    draw1(obst_x, obst_y, height, width1)
-    if count%4==0:
-        speedx=0
-        speedy=obst_speed
-    elif count%3==0: 
-        speedx=obst_speed
-        speedy=0
-    elif count%2==0:
-        speedx=0
-        speedy=obst_speed2
-        pass
-    else:
-        speedy=0
-        speedx=obst_speed2
-
-    obst_y += speedy
-    obst_x += speedx
-    move(x, y)
-
-    if y < obst_y + height  and y > obst_y or y+height > obst_y and y + height < obst_y + height:
-        if x > obst_x and x < obst_x + width1 or x+width > obst_x and x + width < obst_x + width1:
-            # lives-=1
-            # print(lives)
-            # print(lives)
-            # if lives == 0:
-            # print("here")
-            closed = True
-            # pass
-    
-
-
-
-    # reset obst if it goes out of bounds
-    if obst_y > display_height or obst_x > display_width or obst_y < 0 -display_width  or obst_x < 0 - height :
-        count = random.randint(1,4)
-        # print(count)
+        draw1(obst_x, obst_y, height, width1)
         if count%4==0:
-            obst_y = 0 - height
-            obst_x = random.randrange(0,display_width)
-        elif count%3==0:
-            obst_x = 0 - height
-            obst_y = random.randrange(0,display_width)
-        # TODO fix the positions of the right and bottom obstacles
+            speedx=0
+            speedy=obst_speed
+        elif count%3==0: 
+            speedx=obst_speed
+            speedy=0
         elif count%2==0:
-            obst_y = 600
-            obst_x = random.randrange(0,display_width)
+            speedx=0
+            speedy=obst_speed2
             pass
         else:
-            obst_x = 800
-            obst_y = random.randrange(0,display_width)
-            count = 1
-    
-        # if count%4==0:
-        #     obst_speed+=1
-        #     obst_speed2-=1
+            speedy=0
+            speedx=obst_speed2
+
+        obst_y += speedy
+        obst_x += speedx
+        move(x, y)
+
+        if y < obst_y + height  and y > obst_y or y+height > obst_y and y + height < obst_y + height:
+            if x > obst_x and x < obst_x + width1 or x+width > obst_x and x + width < obst_x + width1:
+                closed = True
         
-    
-    pygame.display.update()
-    clock.tick(60)
+        # reset obst if it goes out of bounds
+        if obst_y > display_height or obst_x > display_width or obst_y < 0 -display_width  or obst_x < 0 - height :
+            count = random.randint(1,4)
+
+            if count%4==0:
+                obst_y = 0 - height
+                obst_x = random.randrange(0,display_width)
+            elif count%3==0:
+                obst_x = 0 - height
+                obst_y = random.randrange(0,display_width)
+
+            elif count%2==0:
+                obst_y = 600
+                obst_x = random.randrange(0,display_width)
+                pass
+            else:
+                obst_x = 800
+                obst_y = random.randrange(0,display_width)
+                count = 1
+        
+            # if count%4==0:
+            #     obst_speed+=1
+            #     obst_speed2-=1
+            
+        
+        pygame.display.update()
+        clock.tick(60)
 
 
-pygame.quit()
-quit()
+    pygame.quit()
+    quit()
+
+game_loop()
